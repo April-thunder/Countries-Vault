374ed572a1738a7f85514b10510047c721fc16fd
Alexey Vidyakin <lex_vid@mail.ru>
2022-11-08 20:25:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav-bar v-model:isDarkMode="isDarkMode" />
+  <div class="py-3 w-11/12 lg:w-10/12 mx-auto my-0">
+    <router-view :isDarkMode="isDarkMode"></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      isDarkMode: false,
+    }
+  },
+}
+</script>
---
+++ src/components/CountryList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 justify-center"
+  >
+    <country-item v-for="country in countries" :key="country" :country="country"/>
+  </div>
+</template>
+
+<script>
+import CountryItem from '@/components/CountryItem.vue'
+export default {
+  components: {
+    CountryItem
+  },
+  props: {
+    countries: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
